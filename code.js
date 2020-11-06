7cd36e386a21a9e1ab8d3ca344d991624d4c473c
LittleLink <Simon_0_0_@live.se>
2020-11-06 17:53:52 +0100

    Added date and time module

--- code.js
+++ code.js
@@ -1,8 +1,11 @@
-var http = require('http');
+var http = require('http'); // require initiates a module from node.js
+var dt = require('./customModule');
 var port = 8080;
 
-http.createServer(function (req,res) {
+http.createServer(function (req,res) { // create server with http module
     res.writeHead(200, {'Content-type': 'text/html'});
+    res.write("The date and time is currently: "+ dt.myDateTime());
     res.end('Hello world!');
-}).listen(port);
+}).listen(port); // what port the server should listen to
+
 
