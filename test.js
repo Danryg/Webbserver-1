a8ec1c9fcf06bc2d23ecbeb2e19df7c527edbab8
LittleLink <Simon_0_0_@live.se>
2020-11-07 13:17:07 +0100

    modules

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 console.clear();
 
+var URL;
+var readline = require('readline-sync');
 
+URL = readURL();
 
+function readURL() {
+    return readline.question("URL > ");   
+}
 
-  
+function makeGetRequest() {
+    
+}
+
 
