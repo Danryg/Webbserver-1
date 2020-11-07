cae86fb3dd96a08b05909d75c8f98cf4779a5744
LittleLink <Simon_0_0_@live.se>
2020-11-07 14:02:16 +0100

    Made base program

--- test.js
+++ test.js
@@ -1,16 +1,37 @@
 console.clear();
 
+const express = require('express');
+const request = require('request'); 
+const app = express();
+const port = process.env.PORT = 3000;
+
 var URL;
 var readline = require('readline-sync');
+var info;
 
-URL = readURL();
+/*-------------------------------------*/
+get();
+function get() {
+    URL = readURL();
+    var gitOptions = {
+        url: URL,
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    info = request(gitOptions, function (error, response, body) {
+        console.log(JSON.parse(body));
+        get();
+    });
+}
+
+
+
+
+/*-------------------------------------*/
 
 function readURL() {
     return readline.question("URL > ");   
 }
 
-function makeGetRequest() {
-    
-}
-
-
+app.listen(port, () => console.log(`Listening to port: ${port}.....`));
