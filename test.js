dbd3a5353c0d6314203d8b40f5e695b6856b6300
LittleLink <Simon_0_0_@live.se>
2020-11-07 15:38:59 +0100

    redesign

--- test.js
+++ test.js
@@ -1,35 +1,28 @@
 console.clear();
 
-const express = require('express');
 const request = require('sync-request'); 
-const app = express();
-const port = process.env.PORT = 3000;
+const readline = require('readline-sync');
 
 var URL;
-var readline = require('readline-sync');
 var info;
+var gitOptions = {
+    headers: {
+        'User-Agent': 'request'
+    }
+};
 
 /*-------------------------------------*/
-app.listen(port, () => console.log(`Listening to port: ${port}.....`));
-while (true) {
-    URL = readURL();
-    var gitOptions = {
-        url: URL,
-        headers: {
-            'User-Agent': 'request'
-        }
-    };
-    var info = request('GET', URL, gitOptions);
-    console.log(JSON.parse(info.getBody()));
-    /*info = request(gitOptions, function (error, response, body) {
-        console.log(JSON.parse(body));
-        get();
-    });*/
-}
+URL = readURL();
+var info = getJSON(URL, gitOptions);
+console.log(info["login"]);
 
 /*-------------------------------------*/
 
 function readURL() {
     return readline.question("URL > ");   
+}
+
+function getJSON(URL, options) {
+    return JSON.parse(request('GET', URL, options).getBody());
 }
 
